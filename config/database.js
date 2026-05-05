dd0790c645d558661a66b811a8e63ec7c9b014d7
taqouaa <taqouabehair@gmail.com>
2026-05-05 22:50:04 +0100

    Connect to Supabase cloud database

--- config/database.js
+++ config/database.js
@@ -4,6 +4,12 @@ require('dotenv').config();
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false
+    }
+  }
 });
 
 module.exports = sequelize;
